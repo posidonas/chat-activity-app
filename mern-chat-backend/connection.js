7c4b2ceac531047a3c06495fa6e3159d3dbbabe6
Yiannis <yiannis30@hotmail.com>
2022-11-01 18:32:09 +0200

    removed files

--- mern-chat-backend/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.qg0qj.gcp.mongodb.net/?retryWrites=true&w=majority`, ()=> {
-  console.log('connected to mongodb')
-})
--- mern-chat-backend/routes/roomRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getAppRooms, setAppRoom, updateAppRoom, deleteAppRoom } = require('../controllers/roomController');
-
-router.route('/').get(getAppRooms).post(setAppRoom);
-router.route('/:id').put(updateAppRoom).delete(deleteAppRoom);
-
-module.exports = router
